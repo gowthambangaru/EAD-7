91d2e22b41a319acf2503eec2d3b9b2cf8a31a52
siddu-29 <rallabandisiddhartha4@gmail.com>
2024-11-06 10:01:02 +0530

    inital commit

---
+++ Password-Check-master/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import PasswordStrengthChecker from './PasswordStrengthChecker.js';
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordStrengthChecker />
+    </div>
+  );
+}
+
+export default App;
